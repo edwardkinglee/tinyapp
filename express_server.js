750841405fb56146c3d1fc36a0fe2ca02fecdcb6
edwardkinglee <edward.lee3@telus.com>
2022-11-28 02:22:24 -0500

    added partials folder with _header.ejs and added include

--- express_server.js
+++ express_server.js
@@ -36,7 +36,7 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: '/urls/b2xVn2' };
+  const templateVars = { id: req.params.id, longURL: 'http://www.lighthouselabs.ca' };
   res.render("urls_show", templateVars);
 });
 
