0e0489451bcb3fcf9a7d49e1f91d38a19042d828
edwardkinglee <edward.lee3@telus.com>
2022-11-29 17:47:13 -0500

    added user Object and created a post /register endpoint

--- express_server.js
+++ express_server.js
@@ -24,6 +24,19 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -103,5 +116,17 @@ app.post('/logins', (req, res) => {
 
 app.post('/logout', (req, res) => {
   res.clearCookie('username');
+  res.redirect('/urls');
+});
+
+app.post('/register', (req, res) => {
+  const { email, password } = req.body;
+  const userId = generateRandomString();
+  users[userId] = {
+    id: userId,
+    email,
+    password
+  };
+  res.cookie('username', userId);
   res.redirect('/urls');
 });
