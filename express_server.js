d1687d8f021ae36d55ca713d3736733f02b8d97c
edwardkinglee <edward.lee3@telus.com>
2022-11-29 16:25:49 -0500

    added cookie-parser and res.cookie

--- express_server.js
+++ express_server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // eslint-disable-next-line func-style
 function generateRandomString() {
@@ -44,16 +46,17 @@ app.get("/set", (req, res) => {
 // });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"] };
   console.log(templateVars);
   res.render("urls_show", templateVars);
 });
@@ -86,4 +89,10 @@ app.post("/urls/:id/edit", (req, res) => {
 app.post("/urls/:id", (req, res) => {
   urlDatabase[req.params.id] = req.body['longURL'];
   res.redirect(`/urls`);
+});
+
+app.post('/logins', (req, res) => {
+  // Cookies that have not been signed
+  res.cookie('username',req.body['username']);
+  res.redirect('/urls');
 });
