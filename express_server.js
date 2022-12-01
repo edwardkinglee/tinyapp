4cf20ba33d814a3972e4fc6cd602a593bad62057
edwardkinglee <edward.lee3@telus.com>
2022-11-30 23:51:43 -0500

    updated to require cookie-session, removed cookie and updated with session

--- express_server.js
+++ express_server.js
@@ -1,13 +1,28 @@
+/* eslint-disable camelcase */
 const express = require("express");
-const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
 const { restart } = require("nodemon");
 const app = express();
 const PORT = 8080; // default port 8080
 const bcrypt = require("bcryptjs");
+const bodyParser = require('body-parser');
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+// app.use(cookieParser());
+// app.use(cookieSession({
+//   name: 'session',
+//   keys: ['test'],
+
+//   // Cookie Options
+//   maxAge: 24 * 60 * 60 * 1000 // 24 hours
+// }));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieSession({
+  name: 'session',
+  keys: ['secret', 'rotation']
+}));
 
 // eslint-disable-next-line func-style
 function generateRandomString() {
@@ -82,13 +97,17 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const userId = req.cookies["user_id"];
+  
+  const userId = req.session.user_id;
   let userEmail = '';
   if (users[userId]) {
     userEmail = users[userId]['email'];
   }
+  console.log('Session userId in urls', req.session.user);
+  console.log('userLookup in urls',userLookup(userEmail));
   //if userId doesn't exist or there is no entry in user database
   if (!userId || !userLookup(userEmail)) {
+    console.log('being redirected');
     return res.redirect('/login');
   }
  
@@ -97,7 +116,7 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  const userId = req.cookies["user_id"];
+  const userId = req.session.user_id;
   let userEmail = '';
   if (users[userId]) {
     userEmail = users[userId]['email'];
@@ -112,7 +131,7 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/register", (req, res) => {
-  const userId = req.cookies["user_id"];
+  const userId = req.session.user_id;
   let userEmail = '';
   if (users[userId]) {
     userEmail = users[userId]['email'];
@@ -121,12 +140,12 @@ app.get("/register", (req, res) => {
   if (userId && userLookup(userEmail)) {
     return res.redirect('/urls');
   }
-  const templateVars = { user: users[req.cookies["user_id"]] };
+  const templateVars = { user: users[req.session.user_id] };
   res.render("urls_register", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const userId = req.cookies["user_id"];
+  const userId = req.session.user_id;
   let userEmail = '';
   if (users[userId]) {
     userEmail = users[userId]['email'];
@@ -141,7 +160,7 @@ app.get("/urls/:id", (req, res) => {
     return res.status(400).send('You must be logged as a authorized user');
   }
 
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]['longURL'], user: users[req.cookies["user_id"]] };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]['longURL'], user: users[req.session.user_id] };
   res.render("urls_show", templateVars);
 });
 
@@ -154,8 +173,10 @@ app.get("/u/:id", (req, res) => {
 });
 
 app.get('/login', (req, res) => {
-  console.log(urlDatabase);
-  const userId = req.cookies["user_id"];
+  console.log(users);
+  console.log('session id in login',req.session.user_id);
+ 
+  const userId = req.session.user_id;
   let userEmail = '';
   if (users[userId]) {
     userEmail = users[userId]['email'];
@@ -164,7 +185,7 @@ app.get('/login', (req, res) => {
   if (userId && userLookup(userEmail)) {
     return res.redirect('/urls');
   }
-  const templateVars = { user: users[req.cookies["user_id"]] };
+  const templateVars = { user: users[req.session.user_id] };
   res.render('urls_login', templateVars);
 });
 
@@ -173,19 +194,19 @@ app.listen(PORT, () => {
 });
 
 app.post("/urls", (req, res) => {
-  if (!req.cookies["user_id"]) {
+  if (!req.session.user_id) {
     return res.status(400).send('Must be logged in to shorten URLs');
   }
   let randomString = generateRandomString();
   urlDatabase[randomString] = {
     longURL: req.body['longURL'],
-    userID: req.cookies["user_id"]
+    userID: req.session["user_id"]
   };
   res.redirect(`/urls/${randomString}`); // Replaced to redirect after post response
 });
 
 app.post("/urls/:id/delete", (req, res) => {
-  const userId = req.cookies["user_id"];
+  const userId = req.session.user_id;
   const databaseId = urlDatabase[req.params.id]['userID'];
   if (databaseId !== userId) {
     return res.status(400).send('You must be logged as a authorized user');
@@ -196,7 +217,7 @@ app.post("/urls/:id/delete", (req, res) => {
 });
 
 app.post("/urls/:id/edit", (req, res) => {
-  const userId = req.cookies["user_id"];
+  const userId = req.session.user_id;
   const databaseId = urlDatabase[req.params.id]['userID'];
   if (databaseId !== userId) {
     return res.status(400).send('You must be logged as a authorized user');
@@ -211,12 +232,12 @@ app.post("/urls/:id", (req, res) => {
 });
 
 app.post('/logins', (req, res) => {
-  res.cookie('user_id',req.body['user_id']);
+  req.session.user_id = req.body['user_id'];
   res.redirect('/urls');
 });
 
 app.post('/logout', (req, res) => {
-  res.clearCookie('user_id');
+  req.session = null;
   res.redirect('/login');
 });
 
@@ -238,16 +259,16 @@ app.post('/register', (req, res) => {
     email,
     password: hashedPassword
   };
-  res.cookie('user_id', userId);
+  req.session.user_id = userId;
   res.redirect('/urls');
 });
 
 app.post('/login', (req, res) => {
   const { email, password } = req.body;
   const userId = userLookup(email);
-  console.log('user in login ', userId);
+  
   if (userId && bcrypt.compareSync(password, users[userId].password)) {
-    res.cookie('user_id', userId);
+    req.session.user_id = userId;
     return res.redirect('/urls');
   }
   if (userId && !bcrypt.compareSync(password, users[userId].password)) {
